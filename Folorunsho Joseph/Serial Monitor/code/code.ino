1249fb24df29a74a22047880cfab3a3e6e6def7c
folorunsho joseph <78592746+FolorunshoJoseph@users.noreply.github.com>
2021-02-08 07:44:33 +0100

    Add files via upload

---
+++ Folorunsho Joseph/Serial Monitor/code/code.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello, My name is FOLORUNSHO JOSEPH, 16CJ020726");
+  delay(3000);
+}
